aad428a3fee9ec7e27e791bb48e24ca539d48934
Jagdeep Singh <JS127@atcltd.atc.org.nz>
2021-02-25 11:57:48 +1300

    Add project files.

---
+++ RazorPagesMovie/Migrations/20210224225614_InitialCreat2.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RazorPagesMovie.Migrations
+{
+    public partial class InitialCreat2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Price",
+                table: "Movie",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Movie",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(int));
+        }
+    }
+}
